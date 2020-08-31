c8ec6614b8e543ed7998fd29ad75430cce9054c8
vaishnavi-kamineni13 <vaishnavikamineni@gmail.com>
2020-08-31 15:27:57 +0530

    heroku

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Books,Stationery,Sports,Electronics,Others,Favourites
 
 # Register your models here.
+admin.site.register(Books)
+admin.site.register(Electronics)
+admin.site.register(Stationery)
+admin.site.register(Sports)
+admin.site.register(Others)
+admin.site.register(Favourites)
